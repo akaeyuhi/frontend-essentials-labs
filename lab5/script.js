8a82b3c5e6215a85019a4498ad69878bf8a96c15
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-10-27 19:50:12 +0300

    Initial script config

---
+++ lab5/script.js
@@ -0,0 +1,7 @@
+"use strict";
+import Validator from "./src/validator";
+import Colors from "./src/colors";
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hello world");
+});
