5bf6f68e60d09c4ac878fa461c2f942293bd3843
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-10-28 16:47:03 +0300

    Added colorTable Instance

--- lab5/script.js
+++ lab5/script.js
@@ -4,4 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".form");
   const formOutput = document.querySelector(".form__result");
   const validator = new Validator(form, formOutput);
+  const table = document.querySelector(".table");
+  const picker = document.querySelector(".color-picker");
+  const colorTable = new ColorTable(table, picker);
 });
