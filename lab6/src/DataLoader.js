752577d261f98e682bdcf1ae629da94e435197d0
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-11-07 19:07:48 +0200

    Completed DataLoader class

--- lab6/src/DataLoader.js
+++ lab6/src/DataLoader.js
@@ -4,27 +4,60 @@ class DataLoader {
 
   constructor(rootElement) {
     this._rootElement = rootElement;
+    document
+      .querySelector(".load__button")
+      .addEventListener("click", async () => await this._onClickHandler());
   }
 
-  async loadData() {
+  async _loadData() {
     try {
       const result = await (await fetch("https://randomuser.me/api")).json();
-      const { picture, gender, name, city, postcode, coordinates } = result;
+      const { picture, gender, name, location } = result.results.pop();
       const dto = new UserDto({
-        picture,
+        picture: picture.large,
         gender,
-        name,
-        city,
-        postcode,
-        coordinates,
+        name: name.first + " " + name.last,
+        city: location.city,
+        postcode: location.postcode,
+        coordinates: `latitude: ${location.coordinates.latitude}
+        longitude: ${location.coordinates.longitude}
+        `,
       });
+      this._items.push(dto);
       return dto;
     } catch (error) {
       alert(`Error: ${error.message}`);
     }
   }
 
-  getMarkup(data) {
+  async _loadTableItems(count) {
+    for (let i = 0; i < count; i++) {
+      await this._loadData();
+    }
+    this._setSuccess();
+  }
+
+  async renderResult(count) {
+    await this._loadTableItems(count);
+    for (const item of this._items) {
+      const newElement = document.createElement("div");
+      newElement.innerHTML = this._getMarkup(item);
+      this._rootElement.appendChild(newElement);
+    }
+    this._items = [];
+  }
+
+  async _onClickHandler() {
+    if (!document.querySelector(".load__success").classList.contains("display"))
+      this._setSuccess();
+    await this.renderResult(5);
+  }
+
+  _setSuccess() {
+    document.querySelector(".load__success").classList.toggle("display");
+  }
+
+  _getMarkup(data) {
     return `
       <div class="result__item">
          <img src=${data.picture} alt="image" class="result__image">
