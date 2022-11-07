aa40d7199b97d8efc6c3c5abc32b898a7a7259f5
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-11-07 19:07:25 +0200

    added DataLoader instance

--- lab6/script.js
+++ lab6/script.js
@@ -1,3 +1,6 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", async () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+  const rootElement = document.querySelector(".result__container");
+  const dataLoader = new DataLoader(rootElement);
+});
