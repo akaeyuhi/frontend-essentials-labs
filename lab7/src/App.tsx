0830dc11dd93d9670a54b42273a4fc0f22144971
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-11-24 18:02:11 +0200

    Added GoodsPage to App

--- lab7/src/App.tsx
+++ lab7/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router';
 import { Navbar } from './components/Navbar';
 import AboutPage from './pages/AboutPage';
+import GoodsPage from './pages/GoodsPage';
 
 function App() {
   const navigate = useNavigate();
@@ -12,7 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/about' element={<AboutPage />}/>
-        <Route path='/goods' />
+        <Route path='/goods' element={<GoodsPage />}/>
         <Route path='*' element={<div>404 not found</div>}/>
       </Routes>
     </div>
