c4e3cf95a7a360ccef16fad703c8c6d1b3ef3547
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-11-24 16:02:27 +0200

    Added router to App.tsx

--- lab7/src/App.tsx
+++ lab7/src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router';
 import './App.css';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => navigate('/about'), []);
   return (
-    <div className="App">
+    <div className="container">
+      <Routes>
+        <Route path='/about' />
+        <Route path='/goods' />
+      </Routes>
     </div>
   );
 }
