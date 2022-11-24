07453e8e726cf7a36cb1e3f8b3d130b23c9ca545
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-11-24 16:55:50 +0200

    Added AboutPage and 404

--- lab7/src/App.tsx
+++ lab7/src/App.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router';
 import { Navbar } from './components/Navbar';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   const navigate = useNavigate();
 
   useEffect(() => navigate('/about'), []);
   return (
-    <div className="container">
+    <div className="App">
       <Navbar />
       <Routes>
-        <Route path='/about' />
+        <Route path='/about' element={<AboutPage />}/>
         <Route path='/goods' />
+        <Route path='*' element={<div>404 not found</div>}/>
       </Routes>
     </div>
   );
